eab05d90cc78fe25ef03675f1011e1858cf556c3
ColterLong <colterlong@protonmail.com>
2024-01-06 22:05:10 -0500

    more css

--- src/components/Quote.vue
+++ src/components/Quote.vue
@@ -9,6 +9,7 @@ defineProps({
 <template>
   <main class="quote">
     <h1 class="title">{{ name }}</h1>
+    <hr class="line"/>
     <p class="text">{{ text }}</p>
   </main>
 </template>
@@ -22,10 +23,17 @@ defineProps({
   margin: 16px;
 
 }
+.quote:hover {
+  background-color: #547296;
+}
 /* .title {
   color: #9fd3c7;
 } */
 .title, .text {
   color: #ececec;
+}
+
+.line {
+  border-color: #9fd3c7;
 }
 </style>
